084633200533efffb6ab1e1bb71f5c34cf9cefe5
andma <andreas.ma@smail.th-koeln.de>
2022-07-05 13:18:57 +0200

    test:html implementation

--- public/sketch.js
+++ public/sketch.js
@@ -2,65 +2,9 @@ let materialCanvas;
 let w = 800;
 let h = 800;
 let p5Canvas;
-let requestedData;
-let dataArray = [];
-let dataMin;
-let dataMax;
-let marker;
-let dashboardmarker;
-let scene;
-let camera
-let pipe;
-let cylinderGroup;
-let ball1;
-let ball2;
 
 
 function preload() {
-    //scene = document.querySelector('a-scene');
-
-    scene = document.createElement('a-scene')
-    scene.setAttribute('embedded','');
-    scene.setAttribute('arjs'," detectionMode: mono_and_matrix; matrixCodeType: 3x3;")
-
-
-    ball1 = document.createElement('a-sphere');
-    cylinderGroup= document.createElement('a-entity');
-    ball2= document.createElement('a-sphere');
-    pipe = document.createElement('a-entity');
-    camera= document.createElement('a-camera');
-    marker = document.createElement('a-marker');
-    dashboardmarker = document.createElement('a-marker');
-
-
-    ball1.setAttribute('radius',"0.1");
-    ball2.setAttribute('radius',"0.1");
-
-    //scene.appendChild(camera);
-    cylinderGroup.setAttribute('id',"cylinderGroup")
-
-    marker.setAttribute('id',"m0")
-    marker.setAttribute('type', "barcode");
-    marker.setAttribute('value', "0");
-    marker.setAttribute('registerevents','')
-
-    dashboardmarker.setAttribute('id',"m1")
-    dashboardmarker.setAttribute('type', "barcode");
-    dashboardmarker.setAttribute('value', "1");
-    dashboardmarker.setAttribute('registerevents','')
-
-    pipe.setAttribute('connect','');
-
-    scene.addEventListener('loaded',() => {
-
-    });
-    document.body.appendChild(scene);
-    scene.appendChild(marker);
-    scene.appendChild(dashboardmarker);
-    scene.appendChild(pipe);
-    marker.appendChild(cylinderGroup);
-    marker.appendChild(ball1);
-    dashboardmarker.appendChild(ball2);
 
 }
 
