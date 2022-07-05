07dc8c3e8f3c14f5d1b86bb259a5c973e483312c
andma <andreas.ma@smail.th-koeln.de>
2022-07-05 13:54:46 +0200

    test: revert to js

--- public/init.js
+++ public/init.js
@@ -9,22 +9,22 @@ let ball1;
 let ball2;
 scene = document.createElement('a-scene')
 scene.setAttribute('embedded','');
-scene.setAttribute('arjs'," detectionMode: mono_and_matrix; matrixCodeType: 3x3;")
+scene.setAttribute('arjs',"detectionMode: mono_and_matrix; matrixCodeType: 3x3;")
 
 
 ball1 = document.createElement('a-sphere');
 cylinderGroup= document.createElement('a-entity');
 ball2= document.createElement('a-sphere');
 pipe = document.createElement('a-entity');
-camera= document.createElement('a-camera');
+camera= document.createElement('a-entity');
 marker = document.createElement('a-marker');
 dashboardmarker = document.createElement('a-marker');
+camera.setAttribute('camera','');
 
 
 ball1.setAttribute('radius',"0.1");
 ball2.setAttribute('radius',"0.1");
 
-//scene.appendChild(camera);
 cylinderGroup.setAttribute('id',"cylinderGroup")
 
 marker.setAttribute('id',"m0")
@@ -39,13 +39,11 @@ dashboardmarker.setAttribute('registerevents','')
 
 pipe.setAttribute('connect','');
 
-scene.addEventListener('loaded',() => {
-
-});
 document.body.appendChild(scene);
 scene.appendChild(marker);
 scene.appendChild(dashboardmarker);
 marker.appendChild(cylinderGroup);
-scene.appendChild(pipe);
 marker.appendChild(ball1);
 dashboardmarker.appendChild(ball2);
+scene.appendChild(camera);
+scene.appendChild(pipe);
