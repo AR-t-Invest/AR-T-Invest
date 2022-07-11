6b256bdf6add58ec65f74f3e41e44d8c342e417e
andma <andreas.ma@smail.th-koeln.de>
2022-07-11 18:04:02 +0200

    feat:setText in info.js

--- public/info.js
+++ public/info.js
@@ -10,8 +10,8 @@ class Info {
 
         this.linie.setAttribute("line" , {start: "0.5 2 0", end: "1.5 2 0 ", color: "white"})
         //this.linieZwei.setAttribute("line" ,{start: "1.5 2 0", end: " 2.5 1 -5", color: "blue"})
-
-        this.infoText.setAttribute("value" , "Luftqualitaetssensor \n Marke: Airthings \n")
+this.textValue = "";
+        this.infoText.setAttribute("value" , this.textValue+ "\n Marke: Airthings \n")
         this.infoText.setAttribute("shader", "msdf")
         this.infoText.setAttribute("font", "https://raw.githubusercontent.com/etiennepinchon/aframe-fonts/master/fonts/montserrat/Montserrat-Bold.json")
         this.infoText.setAttribute("scale", "0.5 1 1");
@@ -24,5 +24,9 @@ class Info {
        // marker.appendChild(this.linieZwei)
         marker.appendChild(this.infoText)
     }
-  
+
+    setText(text){
+        this.textValue = text;
+        this.infoText.setAttribute("value" , this.textValue+ "\n Marke: Airthings \n")
+    }
 }
